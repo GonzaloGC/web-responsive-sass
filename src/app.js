d52a5e30ebe5f8edbe36a02e7b0627214acd9fb6
Gonzalo Guerrero <contacto@gonzaloelias.me>
2020-09-07 03:33:47 -0300

    logo and nav

--- src/app.js
+++ src/app.js
@@ -1,8 +1,2 @@
-/* Otra forma de ejecutar el archiivo .scss es a traves de :
-    require ('./styles/main.scss'); */
 import "./styles/main.scss";
-import "./static/img/logo.png";
-
-/* document.body.innerHTML = '<h1>Nuevo texo con javascript</h1>'; */
-
-console.log('Bienvenidos');
+import "./static/img/logo-bl.png";
